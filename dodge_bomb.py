855aa0c2bf80fd489a20a01b7c3746ecb4a4fdad
C0B23046 <c0b23046c1@edu.teu.ac.jp>
2024-06-18 17:52:15 +0900

    さいしゅう #1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -39,7 +39,7 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     if rct.top < 0 or HEIGHT < rct.bottom:
         tate = False
     return yoko, tate
-
+#爆弾の拡大、加速度の段階
 def bomb_accs() -> tuple:
     accs = [a for a in range(1, 11)]
     bb_imgs = []
@@ -57,7 +57,7 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
-    bb_imgs, bb_accs = accl()
+    bb_imgs, bb_accs = bomb_accs()
     bb_img = pg.Surface((20,20))  # 1辺の20の空のSurfaceを作る
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 空のSurfaceに赤い円を描く
     bb_img.set_colorkey((0,0,0))
@@ -73,17 +73,14 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        #衝突判定
+        # 衝突判定
         if kk_rct.colliderect(bb_rct):
-            return  #gameover
-        
-        
-
+            return  # gameover
+    
         screen.blit(bg_img, [0, 0]) 
-
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        #方向キー移動
+        # 方向キー移動
         for k, v in diff.items():
             if key_lst[k]:
                 sum_mv[0] += v[0]
@@ -92,11 +89,12 @@ def main():
         img_dict = dict_i()
         sum_tuple = tuple(sum_mv)
         kk_img = img_dict[sum_tuple]
-
+        # 画面端際のbombの挙動
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         
+        # 爆弾の拡大、加速度
         avx = vx*bb_accs[min(tmr//500, 9)]
         avy = vy*bb_accs[min(tmr//500, 9)]
         bb_img = bb_imgs[min(tmr//500, 9)]
@@ -105,9 +103,9 @@ def main():
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(avx,avy)
         yoko, tate = check_bound(bb_rct)
-        if not yoko: #横方向にはみ出たら
+        if not yoko: # 横方向にはみ出たら
             vx *= -1
-        if not tate: #縦方向にはみ出たら
+        if not tate: # 縦方向にはみ出たら
             vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
