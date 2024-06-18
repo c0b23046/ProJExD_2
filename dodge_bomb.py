c4bbefcebd269f0926e8d1d150b66d61bc21e7cd
C0B23046 <c0b23046c1@edu.teu.ac.jp>
2024-06-18 16:44:28 +0900

    えんしゅう１ がぞうきりかえ

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,6 +3,8 @@ import sys
 import pygame as pg
 import random
 
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 WIDTH, HEIGHT = 1600, 900
 diff = {  #移動量辞書
     pg.K_UP: (0, -5),
@@ -10,7 +12,19 @@ diff = {  #移動量辞書
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (5, 0),
 }
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+def dict_i():
+    image_dict = {  #画像辞書
+        (0, 0): pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0),
+        (5, 0): pg.transform.flip(pg.transform.rotozoom(pg.image.load("fig/3.png"), 180, 2.0), False, True),
+        (5, 5): pg.transform.flip(pg.transform.rotozoom(pg.image.load("fig/3.png"), 225, 2.0), False, True),
+        (0, 5): pg.transform.rotozoom(pg.image.load("fig/3.png"), 90, 2.0),
+        (-5, 5): pg.transform.rotozoom(pg.image.load("fig/3.png"), 45, 2.0),
+        (-5, 0): pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0),
+        (-5, -5): pg.transform.rotozoom(pg.image.load("fig/3.png"), 315, 2.0),
+        (0, -5): pg.transform.rotozoom(pg.image.load("fig/3.png"), 270, 2.0),
+        (5, -5): pg.transform.flip(pg.transform.rotozoom(pg.image.load("fig/3.png"), 135, 2.0), False, True),                                
+    }
+    return image_dict
 
 #画面端の判定
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
@@ -36,12 +50,12 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
-    bb_img = pg.Surface((20,20))
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img = pg.Surface((20,20))  # 1辺の20の空のSurfaceを作る
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 空のSurfaceに赤い円を描く
     bb_img.set_colorkey((0,0,0))
-    bb_rct = bb_img.get_rect()
+    bb_rct = bb_img.get_rect()  # 爆弾Rect
     kk_rct.center = random.randint (0, WIDTH), random.randint (0,HEIGHT)
-    vx, vy = +5 ,+5
+    vx, vy = +5 ,+5  # 爆弾の横方向速度、縦方向速度
     clock = pg.time.Clock()
     tmr = 0
     
@@ -63,9 +77,15 @@ def main():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
+        
+        img_dict = dict_i()
+        sum_tuple = tuple(sum_mv)
+        kk_img = img_dict[sum_tuple]
+
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx,vy)
         yoko, tate = check_bound(bb_rct)
